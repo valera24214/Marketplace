2ca9ac8a87a7f0f519a8f1a8d441f6df8266dc61
User <valera.astroukh@gmail.com>
2025-06-30 10:29:54 +0300

    AuthController added

---
+++ AuthorizationService/Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using AuthorizationService.Models.DTO;
+using AuthorizationService.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthorizationService.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly AuthService authService;
+
+        public AuthController(AuthService authService) => this.authService = authService;
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDto dto)
+        {
+            try 
+            {
+                var token = await authService.Register(dto);
+                return Ok(new {Token = token });
+            }
+            catch(Exception ex) 
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        public async Task<IActionResult> Login(LoginDto dto)
+        {
+            try
+            {
+                var token = await authService.Login(dto);
+                return Ok(new { Token = token });
+            }
+            catch (Exception ex)
+            {
+                return Unauthorized(ex.Message);
+            }
+        }
+    }
+}
